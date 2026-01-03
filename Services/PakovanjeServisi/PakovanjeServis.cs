112f16a71f56aef83b6315dd42c239a3a6512d5a
DanezZ22 <dankomarkovic20042@gmail.com>
2026-01-03 13:19:27 +0100

    Zavrsio PakovanjeServis.cs

---
+++ Services/PakovanjeServisi/PakovanjeServis.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interfaci;
+using Domain.Modeli;
+
+namespace Services.PakovanjeServisi
+{
+    public class PakovanjeServis : IPakovanjeServis
+    {
+        public Paleta PakujVino(long idVinskogPodruma, string adresaOdredista, List<long> idVina)
+        {
+            
+        }
+
+        public bool PosaljiPaletuUPodrum(long idPalete)
+        {
+            
+        }
+    }
+}
